e16cb8c1e4ba569a0daaa8d56365ec3b20765788
Vinicius Alves <viniciusalvesms757@gmail.com>
2024-07-22 18:37:11 -0300

    feat: starting spring project

---
+++ src/main/java/dio/springboot/Calculadora.java
@@ -0,0 +1,12 @@
+package dio.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+
+    public int somar(int number1, int number2) {
+        return number1 + number2;
+    }
+
+}
